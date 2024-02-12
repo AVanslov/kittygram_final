6209c22cfbb9d0641cf6f7e2c508f8d9164833bc
Alexander Buchelnikov <a.buchelnikov99@gmail.com>
2024-02-12 09:56:54 +0100

    1 review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,17 @@
 # flake8: noqa
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.detenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.gotenv('ALLOWED_HOSTS').split()
 #'51.250.27.253', '127.0.0.1', 'localhost', 'kittygramforstady.ddns.net'
 
 INSTALLED_APPS = [
@@ -87,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
