147c0c5d1dd6a2e108c73c492b8572de21a30361
AVanslov <a.buchelnikov99@gmail.com>
2024-03-06 10:31:39 +0100

    Add

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.27.253', '127.0.0.1', 'localhost', 'kittygramforstady.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
