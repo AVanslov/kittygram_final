19cd1c43a01444559fb42a753f7ab2d3c2e6a404
AVanslov <a.buchelnikov99@gmail.com>
2024-03-18 15:13:15 +0100

    try getenv

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
