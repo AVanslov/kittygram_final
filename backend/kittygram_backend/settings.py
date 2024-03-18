157a57c8f520ece600910240196aab98ce6aa2cd
AVanslov <a.buchelnikov99@gmail.com>
2024-03-18 14:51:30 +0100

    try split()

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,11 +9,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
